2389ad9e90ab540ca5945fdef3e758d8602fee4e
Randolph Settgast <settgast1@llnl.gov>
2023-09-18 11:50:09 -0700

    define LinearIndex and MultiIndex types. Add some unit testing

---
+++ src/types/MultiIndex.hpp
@@ -0,0 +1,26 @@
+
+#pragma once
+
+#include <utility>
+
+namespace shiva
+{
+
+template< typename BASE_INDEX_TYPE, int NUM_INDICES >
+struct MultiIndex
+{
+  using BaseIndexType = BASE_INDEX_TYPE;
+  BASE_INDEX_TYPE data[NUM_INDICES] = {0};
+};
+
+
+template< typename BASE_INDEX_TYPE, BASE_INDEX_TYPE...RANGES >
+struct MultiIndexRange
+{
+  using BaseIndexType = BASE_INDEX_TYPE;
+  static constexpr int NUM_INDICES = sizeof...(RANGES);
+  static constexpr int ranges[NUM_INDICES] = { RANGES...};
+  BASE_INDEX_TYPE data[NUM_INDICES] = {0};
+};
+
+} // namespace shiva
