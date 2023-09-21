ce9f163e596c4398bb1d166aae83e5bcc260b10a
Randolph Settgast <settgast1@llnl.gov>
2023-09-20 18:26:24 -1000

    Adding basis functions (#6)
    * added LagrangeBasis classes and tests. Added higher order GaussLobatto spacing and quadrature up to 9 points

---
+++ src/discretizations/finiteElementMethod/bases/LagrangeBasis.hpp
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <utility>
+
+namespace shiva
+{
+namespace finiteElement
+{
+
+
+namespace basis
+{
+template< typename REAL_TYPE, int ORDER, template<typename,int> typename SPACING_TYPE  >
+class LagrangeBasis : public SPACING_TYPE< REAL_TYPE, ORDER+1 >
+{
+public:
+  /// The number of support points for the basis
+  constexpr static int order = ORDER;
+  constexpr static int numSupportPoints = ORDER + 1;
+
+  template< int BF_INDEX >
+  constexpr static REAL_TYPE value( const REAL_TYPE coord )
+  {
+    return valueHelper<BF_INDEX>( coord, std::make_integer_sequence<int,numSupportPoints>{} );
+  }
+
+  template< int BF_INDEX >
+  constexpr static REAL_TYPE gradient( const REAL_TYPE coord )
+  {
+    return gradientHelper<BF_INDEX>( coord, std::make_integer_sequence<int,numSupportPoints>{} );
+  }
+
+
+private:
+  template< int BF_INDEX, int FACTOR_INDEX, int DERIVATIVE_INDEX=-1 >
+  constexpr static REAL_TYPE valueFactor( const REAL_TYPE coord )
+  {
+    if constexpr ( BF_INDEX == FACTOR_INDEX || FACTOR_INDEX==DERIVATIVE_INDEX )
+    {
+      return 1.0;
+    }
+    else
+    {
+      return (                                                               coord - SPACING_TYPE< REAL_TYPE, ORDER+1 >::template coordinate<FACTOR_INDEX>() ) /
+             ( SPACING_TYPE< REAL_TYPE, ORDER+1 >::template coordinate<BF_INDEX>() - SPACING_TYPE< REAL_TYPE, ORDER+1 >::template coordinate<FACTOR_INDEX>() );
+    }
+  }
+
+  template< int BF_INDEX, int ... INDICES >
+  constexpr static REAL_TYPE valueHelper( const REAL_TYPE coord,
+                                         std::integer_sequence<int, INDICES...> )
+  {
+      return ( valueFactor<BF_INDEX,INDICES>( coord ) * ... );
+  }
+
+  template< int BF_INDEX, int FACTOR_INDEX>
+  constexpr static REAL_TYPE gradientFactor()
+  {
+    if constexpr ( BF_INDEX == FACTOR_INDEX )
+    {
+      return 0.0;
+    }
+    else
+    {
+      return 1.0 / ( SPACING_TYPE< REAL_TYPE, ORDER+1 >::template coordinate<BF_INDEX>() - SPACING_TYPE< REAL_TYPE, ORDER+1 >::template coordinate<FACTOR_INDEX>() );
+    }
+  }
+
+  template< int BF_INDEX, int DERIVATIVE_INDEX, int ... FACTOR_INDICES >
+  constexpr static REAL_TYPE gradientChainRuleTerm( const REAL_TYPE coord,
+                                                  std::integer_sequence<int, FACTOR_INDICES...> )
+  {
+      return gradientFactor<BF_INDEX,DERIVATIVE_INDEX>() * ( valueFactor<BF_INDEX,FACTOR_INDICES,DERIVATIVE_INDEX>( coord ) * ... );
+  }
+
+  template< int BF_INDEX, int ... DERIVATIVE_INDICES >
+  constexpr static REAL_TYPE gradientHelper( const REAL_TYPE coord,
+                                             std::integer_sequence<int, DERIVATIVE_INDICES...> )
+  {
+      return ( gradientChainRuleTerm<BF_INDEX,DERIVATIVE_INDICES>( coord, std::make_integer_sequence<int,numSupportPoints>{} ) + ... );
+  }
+
+}; // class LagrangeBasis
+
+} // namespace basis
+} // namespace finiteElement
+} // namespace shiva
---
+++ src/discretizations/unitTests/testLagrangeBasis.cpp
@@ -0,0 +1,56 @@
+
+#include "../finiteElementMethod/bases/LagrangeBasis.hpp"
+#include "../spacing/Spacing.hpp"
+#include "../../common/ShivaMacros.hpp"
+
+#include <gtest/gtest.h>
+#include <cmath>
+
+using namespace shiva;
+using namespace shiva::finiteElement::basis;
+
+
+
+template< typename REAL_TYPE, int ORDER, template<typename,int> typename SPACING, int ... BF_INDEX >
+void testBasisCT( REAL_TYPE const coord,
+                  REAL_TYPE const (&referenceValue)[ORDER+1],
+                  REAL_TYPE const (&referenceGradient)[ORDER+1],
+                  std::integer_sequence<int, BF_INDEX...> )
+{
+
+  using BasisType = LagrangeBasis<REAL_TYPE,ORDER,SPACING>;
+
+    double values[6] = { BasisType::template value<BF_INDEX>(coord)... };
+    double gradients[6] = { BasisType::template gradient<BF_INDEX>(coord)... };
+
+  for( int a=0; a<ORDER+1 ; ++a )
+  {
+    EXPECT_NEAR( values[a], referenceValue[a], abs(referenceValue[a]) * 1e-13 );
+    EXPECT_NEAR( gradients[a], referenceGradient[a], abs(referenceGradient[a]) * 1e-13 );
+  }
+
+}
+
+
+TEST( testSpacing, testLagrangeBasis )
+{
+  testBasisCT< double, 5, EqualSpacing >( 0.3, 
+                                          {-0.0076904296875,0.0555419921875,-0.199951171875,0.999755859375,0.1666259765625,-0.0142822265625}, 
+                                          {-0.064208984375,0.44474283854167,-1.42333984375,-0.88134765625,2.0747884114583,-0.150634765625},
+                                          std::make_integer_sequence<int,6>{} );
+
+  testBasisCT< double, 5, GaussLobattoSpacing >( 0.3, 
+                                                {-0.0039331249999999,0.011438606947084,-0.025205197129078,0.99880774621932,0.026196343962672,-0.0073043749999999}, 
+                                                {-0.26265625,0.76193547139425,-1.6595367733139,-0.16178550663066,1.8258868085503,-0.50384375},
+                                                std::make_integer_sequence<int,6>{} );
+
+}
+
+
+
+int main( int argc, char * * argv )
+{
+  ::testing::InitGoogleTest( &argc, argv );
+  int const result = RUN_ALL_TESTS();
+  return result;
+}
