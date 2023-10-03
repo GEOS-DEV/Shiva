684678cb5e0a3445093ae3fe9a16ce1ab192cef9
Randolph Settgast <settgast1@llnl.gov>
2023-10-03 13:48:50 -0700

    added more modifications to SequenceUtilities.hpp

--- src/common/SequenceUtilities.hpp
+++ src/common/SequenceUtilities.hpp
@@ -6,6 +6,7 @@
 namespace shiva
 {
 
+
 namespace detail
 {
 template< typename... INTEGER_SEQUENCES >
@@ -20,55 +21,46 @@ struct SequenceExpansion< std::integer_sequence<int, DIMENSION_INDICES...> >
   {
     if constexpr ( !std::is_invocable_v<FUNC,int,ARGS...> )
     {
-      return func.template operator()<DIMENSION_INDICES...,ARGS...>(std::forward<ARGS>(args)...);
+      return func.template operator()< DIMENSION_INDICES... >(std::forward<ARGS>(args)...);
     }
     else
     {
-      return func(DIMENSION_INDICES...,std::forward<ARGS>(args)...);
-    }
-  }
-
-  template< typename FUNC, typename ... ARGS >
-  constexpr static auto execute( ARGS && ... args )
-  {
-    if constexpr ( std::is_class_v< FUNC >)
-    {
-      return FUNC::template execute<DIMENSION_INDICES...,ARGS...>( std::forward<ARGS>(args)... );
+      return func( std::integral_constant<int,DIMENSION_INDICES>{}...,
+                   std::forward<ARGS>(args)...);
     }
   }
 
   template< typename FUNC >
-  constexpr static void staticFor( FUNC && func )
+  constexpr static auto staticFor( FUNC && func )
   {
     if constexpr ( !std::is_invocable_v<FUNC,int> )
     {
-      (func.template operator()<DIMENSION_INDICES>(),...);
+      return (func.template operator()<DIMENSION_INDICES>(),...);
     }
     else
     {
-      (func(DIMENSION_INDICES),...);
+      return (func( std::integral_constant<int,DIMENSION_INDICES>{} ),...);
     }
   }
 };
 }
 
 template< int END, typename FUNC, typename ... ARGS >
-constexpr auto executeSequence( FUNC&& func, ARGS && ... args )
+constexpr auto executeSequence( FUNC&& func, 
+                                ARGS && ... args )
 {
-  return detail::SequenceExpansion<std::make_integer_sequence<int,END> >::template execute( std::forward<FUNC>(func), std::forward<ARGS>(args)... );
+  return 
+  detail::SequenceExpansion< std::make_integer_sequence<int,END> >::
+  template execute( std::forward<FUNC>(func), 
+                    std::forward<ARGS>(args)... );
 }
-
-template< int END, typename FUNC, typename ... ARGS >
-constexpr auto executeSequence( ARGS && ... args )
-{
-  return detail::SequenceExpansion<std::make_integer_sequence<int,END> >::template execute<FUNC>( std::forward<ARGS>(args)... );
-}
-
 
 template< int END, typename FUNC  >
-constexpr void forSequence( FUNC&& func )
+constexpr auto forSequence( FUNC&& func )
 {
-  detail::SequenceExpansion<std::make_integer_sequence<int,END> >::template staticFor( std::forward<FUNC>(func) );
+  return 
+  detail::SequenceExpansion<std::make_integer_sequence<int,END> >::
+  template staticFor( std::forward<FUNC>(func) );
 }
 
 } // namespace shiva
--- src/common/unitTests/testSequenceUtilities.cpp
+++ src/common/unitTests/testSequenceUtilities.cpp
@@ -7,68 +7,167 @@ using namespace shiva;
 
 
 
-TEST( testSequenceUtilities, testExecuteSequenceLambda )
+TEST( testSequenceUtilities, testSequenceExpansionLambda )
 {
   constexpr int h0[10] = {11,22,33,44,55,66,77,88,99,100};
   const int h1[10] = {11,22,33,44,55,66,77,88,99,100};
-  constexpr int some_of_h = 595;
-
-  constexpr int staticSum0 = executeSequence<10>( 
-  [&]( auto const && ... a ) constexpr
-  {
-    return (h0[a] + ...);
-  });
-  static_assert( staticSum0 == some_of_h );
-
-  int staticSum1 = executeSequence<10>( 
-  [&]( auto const && ... a ) constexpr
-  {
-    return (h1[a] + ...);
-  });
-  EXPECT_EQ( staticSum1, some_of_h );
-}
-
-  struct FUNCTOR0
-  {
-    template< int ... INDICES >
-    static constexpr int execute( int const (&h)[10] )
-    {
-      return (h[INDICES] + ...);
-    }
-  };
-
-TEST( testSequenceUtilities, testExecuteSequenceFunctor )
-{
-  constexpr int h0[10] = {11,22,33,44,55,66,77,88,99,100};
   constexpr int sum_of_h = 595;
 
-  constexpr int staticSum0 = executeSequence<10,FUNCTOR0>( h0 );
+  constexpr int staticSum0 = executeSequence<10>( 
+  [&]( auto const && ... a ) constexpr
+  {
+    return (h0[a] + ...);
+  });
   static_assert( staticSum0 == sum_of_h );
+
+  int staticSum1 = executeSequence<10>( 
+  [&]( auto const && ... a ) constexpr
+  {
+    return (h1[a] + ...);
+  });
+  EXPECT_EQ( staticSum1, sum_of_h );
 }
 
-#if __cplusplus >= 202002L
-TEST( testSequenceUtilities, testExecuteSequenceTemplateLambda )
+TEST( testSequenceUtilities, testNestedSequenceExpansionLambda )
 {
   constexpr int h0[10] = {11,22,33,44,55,66,77,88,99,100};
   const int h1[10] = {11,22,33,44,55,66,77,88,99,100};
-  constexpr int some_of_h = 595;
+  constexpr int sum_of_h = 354025;
+
+  constexpr int staticSum0 = executeSequence<10>(
+  [&]<int ... a>() constexpr
+  {
+    return 
+    ( executeSequence<10>
+      ( 
+        [ h=h0, aa=std::integral_constant<int,a>{} ] ( auto const ... b ) constexpr 
+        { return ( (h[aa]*h[b]) + ...); } 
+      ) + ... 
+    );
+  } );
+  static_assert( staticSum0 == sum_of_h );
+
+  int const staticSum1 = executeSequence<10>(
+  [&]<int ... a>() constexpr
+  {
+    return 
+    ( executeSequence<10>
+      ( 
+        [ h=h1, aa=std::integral_constant<int,a>{} ] ( auto const ... b ) constexpr 
+        { return ( (h[aa]*h[b]) + ...); } 
+      ) + ... 
+    );
+  } );
+  EXPECT_EQ( staticSum1, sum_of_h );
+}
+
+TEST( testSequenceUtilities, testForSequenceLambda )
+{
+  constexpr int h0[10] = {11,22,33,44,55,66,77,88,99,100};
+  const int h1[10] = {11,22,33,44,55,66,77,88,99,100};
+  constexpr int sum_of_h = 595;
+
+  constexpr auto helper = []( auto const & h ) constexpr
+  {
+    int staticSum0 = 0;
+    forSequence<10>( 
+    [&]( auto const a ) constexpr
+    {
+      staticSum0 += h[a];
+    });
+    return staticSum0;
+  };
+
+  constexpr int staticSum0 = helper( h0 );
+  static_assert( staticSum0 == sum_of_h );
+
+  int const staticSum1 = helper( h1 );
+  EXPECT_EQ( staticSum1, sum_of_h );
+}
+
+
+
+#if __cplusplus >= 202002L
+TEST( testSequenceUtilities, testSequenceExpansionTemplateLambda )
+{
+  constexpr int h0[10] = {11,22,33,44,55,66,77,88,99,100};
+  const int h1[10] = {11,22,33,44,55,66,77,88,99,100};
+  constexpr int sum_of_h = 595;
 
   constexpr int staticSum0 = executeSequence<10>( 
   [&]<int ... a>() constexpr
   {
     return (h0[a] + ...);
   });
-  static_assert( staticSum0 == some_of_h );
+  static_assert( staticSum0 == sum_of_h );
 
   int staticSum1 = executeSequence<10>( 
   [&]<int ... a>() constexpr
   {
     return (h1[a] + ...);
   });
-  EXPECT_EQ( staticSum1, some_of_h );
+  EXPECT_EQ( staticSum1, sum_of_h );
+}
 
+
+TEST( testSequenceUtilities, testNestedSequenceExpansionTemplateLambda )
+{
+  constexpr int h0[10] = {11,22,33,44,55,66,77,88,99,100};
+  const int h1[10] = {11,22,33,44,55,66,77,88,99,100};
+  constexpr int sum_of_h = 354025;
+
+  constexpr int staticSum0 = executeSequence<10>(
+  [&]<int ... a>() constexpr
+  {
+    return 
+    ( executeSequence<10>
+      ( 
+        [ h=h0, aa=std::integral_constant<int,a>{} ] <int ... b>() constexpr 
+        { return ( (h[aa]*h[b]) + ...); } 
+      ) + ... 
+    );
+  } );
+  static_assert( staticSum0 == sum_of_h );
+
+  int const staticSum1 = executeSequence<10>(
+  [&]<int ... a>() constexpr
+  {
+    return 
+    ( executeSequence<10>
+      ( 
+        [ h=h1, aa=std::integral_constant<int,a>{} ] <int ... b>() constexpr 
+        { return ( (h[aa]*h[b]) + ...); } 
+      ) + ... 
+    );
+  } );
+  EXPECT_EQ( staticSum1, sum_of_h );
+}
+
+TEST( testSequenceUtilities, testForSequenceTemplateLambda )
+{
+  constexpr int h0[10] = {11,22,33,44,55,66,77,88,99,100};
+  const int h1[10] = {11,22,33,44,55,66,77,88,99,100};
+  constexpr int sum_of_h = 595;
+
+  constexpr auto helper = []( auto const & h ) constexpr
+  {
+    int staticSum0 = 0;
+    forSequence<10>( 
+    [&]<int a>() constexpr
+    {
+      staticSum0 += h[a];
+    });
+    return staticSum0;
+  };
+
+  constexpr int staticSum0 = helper( h0 );
+  static_assert( staticSum0 == sum_of_h );
+
+  int const staticSum1 = helper( h1 );
+  EXPECT_EQ( staticSum1, sum_of_h );
 }
 #endif
+
 
 int main( int argc, char * * argv )
 {
