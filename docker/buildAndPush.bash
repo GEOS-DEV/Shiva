778ef8fe80b41ca07873a005eb5f671130a25b50
Randolph R. Settgast1 <settgast1@streak.llnl.gov>
2023-10-22 00:44:47 -0700

    fix docker script issues

--- docker/buildAndPush.bash
+++ docker/buildAndPush.bash
@@ -2,17 +2,23 @@
 
 
 
-images="ubi8:ubi8.8"
+images="ubi8:ubi8.8-854"
+#  ubi8-cuda11:ubi8.8-cuda11.8.0  ubi8-cuda12:ubi8.8-cuda12.2.2
 
 for image in $images; do
   imagePart=(${image//:/ })
   dir=${imagePart[0]}
   tag=${imagePart[1]}
-  repo=geosx/${imagePart[0]}:${tag}
-  echo ${repo}
 
-  docker build ${dir} -t ${tag}
-  docker push ${repo}
+  
+  repo=geosx/ubi8:${tag}
+
+  echo dir=${dir}
+  echo repo=${repo}
+
+
+docker build ${dir} -t ${repo}
+docker push ${repo}
 done
 
 
