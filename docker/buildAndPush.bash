1a04896c0960678f18eaa72cf107e481a60f00f7
Randolph Settgast <settgast1@llnl.gov>
2023-10-22 13:15:09 -0700

    add ubunty22-cuda11 dockerfile

--- docker/buildAndPush.bash
+++ docker/buildAndPush.bash
@@ -2,8 +2,8 @@
 
 
 
-images="ubi8:ubi8.8-854"
-#  ubi8-cuda11:ubi8.8-cuda11.8.0  ubi8-cuda12:ubi8.8-cuda12.2.2
+images="ubuntu22-cuda11:ubuntu22.04-cuda11.8.0"
+# ubi8:ubi8.8-854 ubi8-cuda11:ubi8.8-cuda11.8.0  ubi8-cuda12:ubi8.8-cuda12.2.2
 
 for image in $images; do
   imagePart=(${image//:/ })
