fb0064afbdd059b06b0dd05a9186751fcddf0d5d
Randolph Settgast <settgast1@llnl.gov>
2023-10-22 20:20:01 -0700

    update buildAndPush

--- docker/buildAndPush.bash
+++ docker/buildAndPush.bash
@@ -2,7 +2,7 @@
 
 
 
-images="ubuntu:22.04 ubuntu-cuda11:22.04-cuda11.8.0 ubi8:ubi8.8-854 ubi8-cuda11:ubi8.8-cuda11.8.0  ubi8-cuda12:ubi8.8-cuda12.2.2"
+images="ubuntu:22.04 ubuntu-cuda11:22.04-cuda11.8.0 ubi:ubi8.8-854 ubi-cuda11:ubi8.8-cuda11.8.0  ubi-cuda12:ubi8.8-cuda12.2.2"
 
 for image in $images; do
   imagePart=(${image//:/ })
