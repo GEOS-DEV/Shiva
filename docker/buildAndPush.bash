29def714834071fe5e09f523279afcb1085b7c05
Randolph Settgast <settgast1@llnl.gov>
2023-10-22 19:52:42 -0700

    adding doxygen and cppcheck and clang-tidy

--- docker/buildAndPush.bash
+++ docker/buildAndPush.bash
@@ -2,8 +2,7 @@
 
 
 
-images="ubuntu22-cuda11:ubuntu22.04-cuda11.8.0"
-# ubi8:ubi8.8-854 ubi8-cuda11:ubi8.8-cuda11.8.0  ubi8-cuda12:ubi8.8-cuda12.2.2
+images="ubuntu22:22.04 ubuntu22-cuda11:ubuntu22.04-cuda11.8.0 ubi8:ubi8.8-854 ubi8-cuda11:ubi8.8-cuda11.8.0  ubi8-cuda12:ubi8.8-cuda12.2.2"
 
 for image in $images; do
   imagePart=(${image//:/ })
@@ -15,10 +14,12 @@ for image in $images; do
   
   repo=geosx/${dirRoot}:${tag}
 
+  echo 
   echo dir=${dir}
   echo repo=${repo}
+  echo 
 
 
-docker build ${dir} -t ${repo}
-docker push ${repo}
+#docker build ${dir} -t ${repo}
+#docker push ${repo}
 done
