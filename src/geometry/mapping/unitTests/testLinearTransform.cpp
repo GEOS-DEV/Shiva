cdd1c4950b3182549fa2d835dbae1b9a8fd206e2
Randolph Settgast <settgast1@llnl.gov>
2023-12-22 09:37:58 -0800

    add header to each source file

--- src/geometry/mapping/unitTests/testLinearTransform.cpp
+++ src/geometry/mapping/unitTests/testLinearTransform.cpp
@@ -1,3 +1,16 @@
+/*
+ * ------------------------------------------------------------------------------------------------------------
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright (c) 2023  Lawrence Livermore National Security LLC
+ * Copyright (c) 2023  TotalEnergies
+ * Copyright (c) 2023- Shiva Contributors
+ * All rights reserved
+ *
+ * See Shiva/LICENSE, COPYRIGHT, CONTRIBUTORS, NOTICE, and ACKNOWLEDGEMENTS files for details.
+ * ------------------------------------------------------------------------------------------------------------
+ */
+
 
 #include "geometry/shapes/NCube.hpp"
 #include "../LinearTransform.hpp"
