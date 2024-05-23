c15c633b60141431828746fd8eb9c8b10a8b6276
Randolph Settgast <settgast1@llnl.gov>
2024-05-23 09:54:54 -0700

    remove container name from docker run command

--- scripts/ci_build_and_test.sh
+++ scripts/ci_build_and_test.sh
@@ -12,7 +12,7 @@ CONTAINER_NAME=shiva_build
 # Now we can build shiva.
 docker run \
   --rm \
-  --name=${CONTAINER_NAME} \
+#  --name=${CONTAINER_NAME} \
   --volume=${BUILD_DIR}:${BUILD_DIR_MOUNT_POINT} \
   --cap-add=ALL \
   ${DOCKER_RUN_ARGS} \
