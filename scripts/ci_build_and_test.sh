dc7dfeb0371d8d4cc2892b7c94f57760656bd0e6
Randolph Settgast <settgast1@llnl.gov>
2024-05-23 23:10:18 -0700

    add nproc flag to builds

--- scripts/ci_build_and_test.sh
+++ scripts/ci_build_and_test.sh
@@ -5,6 +5,10 @@ BUILD_DIR=${GITHUB_WORKSPACE}
 BUILD_DIR_MOUNT_POINT=/tmp/Shiva
 
 
+if [[ -z "${NPROC}" ]]; then
+  NPROC=$(nproc)
+fi
+
 # We need to keep track of the building container (hence the `CONTAINER_NAME`)
 # so we can extract the data from it later (if needed). Another solution would have been to use a mount point,
 # but that would not have solved the problem for the TPLs (we would require extra action to copy them to the mount point).
@@ -23,5 +27,6 @@ docker run \
   -e ENABLE_CUDA=${ENABLE_CUDA} \
   -e CMAKE_CUDA_ARCHITECTURES=${CMAKE_CUDA_ARCHITECTURES} \
   -e CMAKE_BUILD_TYPE \
+  -e NPROC \
   ${DOCKER_REPOSITORY} \
   ${BUILD_DIR_MOUNT_POINT}/scripts/ci_build_and_test_in_container.sh ${BUILD_AND_TEST_ARGS};
